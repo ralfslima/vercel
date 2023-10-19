b1941700f359c9ac36fe7353ad661a28c986fddd
ralflima <ralf.lima@gmail.com>
2023-10-19 20:15:00 -0300

    teste

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+var path = require('path');
 
 const { engine } = require('express-handlebars');
 
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, '..', 'views', 'layouts')}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
